ded49c3a8facd91789aa49f24c963b8575f7cc28
Shreeya K <skarnam1@uncc.edu>
2023-10-02 16:58:02 -0400

    code

--- 04/gettingStarted.jsx
+++ 04/gettingStarted.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/main.css';
 
+
+import Header from './components/header/Header';
 import Example from './components/example/Example';
 
 ReactDOM.render(
   <Example />,
   document.getElementById('reactapp'),
+);
+
+ReactDOM.render(
+    <Example />,
+    document.getElementById('reactapp')
 );
